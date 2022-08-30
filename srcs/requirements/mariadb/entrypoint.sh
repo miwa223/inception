ff1da386cf4129b296cbf1fd6986492ad99096d6
miwa223 <j.aimeparis93@gmail.com>
2022-08-30 20:11:17 +0200

    add mariadb files

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -2,18 +2,19 @@
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     /etc/init.d/mariadb setup
+    rc-status
     rc-service mariadb start
-    echo "CREATE TABLE '$MyDatabaseName';" >> /tmp/sql
+    echo "CREATE DATABASE '$MyDatabaseName';" >> /tmp/sql
     echo "CREATE USER '$WP_USRE1'@'%' IDENTIFIED BY '$MY_USER1_PW';" >> /tmp/sql
     echo "GRANT ALL ON $MyDatabaseName.* TO '$WP_USER1'@'%' WITH GRANT OPTION;" >> /tmp/sql
     echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PW';" >> /tmp/sql
     echo "FLUSH PRIVILEGES;" >> /tmp/sql
-    cat /tmp/sql | mysql -u root --password="$MyPassWord"
+    cat /tmp/sql | mysql -u root
     rm /tmp/sql
     rc-service mariadb stop
-# else
-#     rc-service mariadb start
-    # rc-service mariadb stop
+else
+    rc-service mariadb start
+    rc-service mariadb stop
 fi
 
-mysql --user=mysql --datadir=/var/lib/mysql
+mysqld --user=mysql --datadir=/var/lib/mysql
