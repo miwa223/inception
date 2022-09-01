b28d3887d0e8ae0e4ee61776a90d359552e73771
miwa223 <j.aimeparis93@gmail.com>
2022-09-01 15:58:12 +0200

    modify env vars

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -4,10 +4,10 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     /etc/init.d/mariadb setup
     rc-status
     rc-service mariadb start
-    echo "CREATE DATABASE '$MyDatabaseName';" >> /tmp/sql
-    echo "CREATE USER '$WP_USRE1'@'%' IDENTIFIED BY '$MY_USER1_PW';" >> /tmp/sql
-    echo "GRANT ALL ON $MyDatabaseName.* TO '$WP_USER1'@'%' WITH GRANT OPTION;" >> /tmp/sql
-    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PW';" >> /tmp/sql
+    echo "CREATE DATABASE '$MYSQL_DB_NAME';" >> /tmp/sql
+    echo "CREATE USER '$MYSQL_DB_USERNAME'@'%' IDENTIFIED BY '$MYSQL_DB_PASSWORD';" >> /tmp/sql
+    echo "GRANT ALL ON $MYSQL_DB_NAME.* TO '$MYSQL_DB_USERNAME'@'%' WITH GRANT OPTION;" >> /tmp/sql
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_DB_ROOT_PASSWORD';" >> /tmp/sql
     echo "FLUSH PRIVILEGES;" >> /tmp/sql
     cat /tmp/sql | mysql -u root
     rm /tmp/sql
