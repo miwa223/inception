116a966dfb5847cf98cc0856afbb860e4d6f53ed
miwa223 <j.aimeparis93@gmail.com>
2022-09-07 15:54:38 +0200

    remove some quote to fix syntax error

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -4,10 +4,13 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     /etc/init.d/mariadb setup
     rc-status
     rc-service mariadb start
-    echo "CREATE DATABASE '$MYSQL_DB_NAME';" >> /tmp/sql
-    echo "CREATE USER '$MYSQL_DB_USERNAME'@'%' IDENTIFIED BY '$MYSQL_DB_PASSWORD';" >> /tmp/sql
-    echo "GRANT ALL ON $MYSQL_DB_NAME.* TO '$MYSQL_DB_USERNAME'@'%' WITH GRANT OPTION;" >> /tmp/sql
-    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_DB_ROOT_PASSWORD';" >> /tmp/sql
+    echo "CREATE DATABASE $MYSQL_NAME;" > /tmp/sql
+    echo "CREATE USER $MYSQL_USER@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> /tmp/sql
+    echo "GRANT ALL ON $MYSQL_NAME.* TO $MYSQL_USER@'%' WITH GRANT OPTION;" >> /tmp/sql
+    # maybe remove later
+    echo "GRANT ALL ON $MYSQL_NAME.* TO $MYSQL_USER@'localhost' WITH GRANT OPTION;" >> /tmp/sql
+    echo "ALTER USER root@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> /tmp/sql
+    echo "GRANT ALL ON *.* TO root@'localhost' WITH GRANT OPTION;" >> /tmp/sql
     echo "FLUSH PRIVILEGES;" >> /tmp/sql
     cat /tmp/sql | mysql -u root
     rm /tmp/sql
@@ -17,4 +20,4 @@ else
     rc-service mariadb stop
 fi
 
-mysqld --user=mysql --datadir=/var/lib/mysql
+exec usr/bin/mysqld --user=mysql --datadir=/var/lib/mysql
